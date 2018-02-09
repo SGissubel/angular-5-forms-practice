9358e55a3ff57ba71d297127c33a9a159e642911
Steven Gissubel <steven.gissubel@gmail.com>
2018-02-08 20:50:50 -0500

    added a user property to ts comp - setting values from form on submit - showing values if available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,45 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('login') singupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secreteQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.singupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+    this.singupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
   onSubmit() {
-    console.log(this.singupForm);
+    this.user.username = this.singupForm.value.username;
+    this.user.email = this.singupForm.value.email;
+    this.user.secreteQuestion = this.singupForm.value.secret;
+    this.user.answer = this.singupForm.value.questionAnswer;
+    this.user.gender = this.singupForm.value.gender;
+    this.submitted = true;
   }
 }
